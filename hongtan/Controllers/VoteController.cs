eceb11deb1023cbe27df77db89d214f564f93843
quillblue <anbiandeyu@gmail.com>
2014-05-12 23:32:27 +0800

    First Commit. Broken. Static Version is under constructed.
    Signed-off-by: quillblue <anbiandeyu@gmail.com>

---
+++ hongtan/Controllers/VoteController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace hongtan.Controllers
+{
+    public class VoteController : Controller
+    {
+        //
+        // GET: /Vote/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
